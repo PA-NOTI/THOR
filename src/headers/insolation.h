627b86ba062ed9d206c64cf1db8fdc5c1df09266
PANOTI <51680261+PA-NOTI@users.noreply.github.com>
2025-04-19 18:40:42 +0200

    moonoo

--- src/headers/insolation.h
+++ src/headers/insolation.h
@@ -166,7 +166,6 @@ private:
     double ecc_host           = 0;        // orbital eccentricity of host planet    
     double ecc_host_config    = 0;        // orbital eccentricity of host planet    
     double r_orb_host         = 1;        // orbital distance/semi-major axis
-    double planetary_Omega = 0;
     
     double table_num_parmentier_config = 2; // if picket-fence, table number used to compute gamma (with/without Tio)
 
