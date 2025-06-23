7da51897ed2e9be06b054038ebb3a7f5d784df2f
PANOTI <51680261+PA-NOTI@users.noreply.github.com>
2025-06-23 11:04:03 +0200

    Update insolation.cu

--- src/ESP/insolation.cu
+++ src/ESP/insolation.cu
@@ -426,19 +426,17 @@ bool Insolation::initialise_memory(const ESP&               esp,
         cos_zenith_angles.allocate(esp.point_num);
         cos_zenith_angles.zero();
         
-        if (esp.moon_irr_mode)
-        {
+        
         cos_zenith_angles_moon.allocate(esp.point_num);
         cos_zenith_angles_moon.zero();
-        }
+        
 
-        if (esp.binary_star_mode)
-        {
-            cos_zenith_angles_S1.allocate(esp.point_num);
-            cos_zenith_angles_S1.zero();
-            cos_zenith_angles_S2.allocate(esp.point_num);
-            cos_zenith_angles_S2.zero();
-        }
+        
+        cos_zenith_angles_S1.allocate(esp.point_num);
+        cos_zenith_angles_S1.zero();
+        cos_zenith_angles_S2.allocate(esp.point_num);
+        cos_zenith_angles_S2.zero();
+        
 
         USE_BENCHMARK();
 
